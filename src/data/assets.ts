d7e5deb2a3dc0f83aef87f43017cbdac803930fb
zhaofengzfw-design <zhaofeng.zfw@gmail.com>
2026-09-06 23:51:04 +0800

    init: src/data/assets.ts

---
+++ src/data/assets.ts
@@ -0,0 +1,14 @@
+// EXPORTS: HERO_BG_URL, CLASS_IMAGES, QR_URL
+export const HERO_BG_URL = 'https://aka.doubaocdn.com/s/Tmzl9X4MIU';
+
+export type CharacterClassName = '战士' | '飞侠' | '魔法师' | '弓箭手' | '海盗';
+
+export const CLASS_IMAGES: Record<CharacterClassName, string> = {
+  '战士': 'https://aka.doubaocdn.com/s/5Waa7X7vcc',
+  '飞侠': 'https://aka.doubaocdn.com/s/mG0NwqyJJz',
+  '魔法师': 'https://aka.doubaocdn.com/s/rfG9976vqB',
+  '弓箭手': 'https://aka.doubaocdn.com/s/h5TXsZIi8o',
+  '海盗': 'https://aka.doubaocdn.com/s/vxgRCWnsW4',
+};
+
+export const QR_URL = 'https://yjdwb.oss-cn-beijing.aliyuncs.com/二维码测试.png';
